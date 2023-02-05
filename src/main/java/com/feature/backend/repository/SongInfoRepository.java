d0f130966f857a668d6efc865f82a666fe16e8bd
rachelxx03 <53119641+rachelxx03@users.noreply.github.com>
2023-02-04 21:18:40 -0400

    hehehe

--- src/main/java/com/feature/backend/repository/SongInfoRepository.java
+++ src/main/java/com/feature/backend/repository/SongInfoRepository.java
@@ -2,6 +2,8 @@ package com.feature.backend.repository;
 
 import com.feature.backend.model.SongInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
@@ -9,4 +11,14 @@ import java.util.List;
 @org.springframework.stereotype.Repository
 
 public interface SongInfoRepository extends JpaRepository<SongInfo,Integer> {
+    @Query("SELECT s FROM SongInfo s WHERE s.year BETWEEN :startYear AND :endYear")
+    List<SongInfo> findByYearBetween(@Param("startYear") int startYear, @Param("endYear") int endYear);
+
+
+    @Query("SELECT s FROM SongInfo s WHERE s.year BETWEEN :startYear AND :endYear")
+   String findLyricsByRankingAndYearBetween( @Param("startYear") int startYear, @Param("endYear") int endYear);
+
+
+    @Query(value = "SELECT GROUP_CONCAT(DISTINCT CONCAT(lyric)) AS combined_A FROM song_info", nativeQuery = true)
+    String concatLyrics();
 }
